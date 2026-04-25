144a587313be7eb27f105ab4089ff9de9266eaf2
Suraj Jha <dnjha240@gmail.com>
2026-04-25 07:43:11 +0000

    fix: remove PILLAR_ORDER import from models/__init__ (was deleted in refactor)

--- src/finagent/models/__init__.py
+++ src/finagent/models/__init__.py
@@ -1,6 +1,6 @@
 from .summary import FinancialSummary
 from .mf import MFHolding, MFTransaction
 from .goal import Goal, GOAL_TEMPLATES
-from .profile import UserProfile, PILLAR_ORDER
+from .profile import UserProfile
 
-__all__ = ["FinancialSummary", "MFHolding", "MFTransaction", "Goal", "GOAL_TEMPLATES", "UserProfile", "PILLAR_ORDER"]
+__all__ = ["FinancialSummary", "MFHolding", "MFTransaction", "Goal", "GOAL_TEMPLATES", "UserProfile"]
